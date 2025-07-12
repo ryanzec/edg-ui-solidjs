114f136a4dbec6500f97a4786ceb6e57a9dd570c
Ryan Zec <basire@gmail.com>
2025-07-12 18:02:37 -0400

    remove obsolete table component (grid table replaces it)

--- packages/core/components/table/index.ts
+++
@@ -1,9 +0,0 @@
-import Table, { type TableProps } from '$/core/components/table/table';
-import Data, { type TableDataProps } from '$/core/components/table/table-data';
-import Header, { type TableHeaderProps } from '$/core/components/table/table-header';
-import Row, { type TableRowProps } from '$/core/components/table/table-row';
-
-export { TableShape } from '$/core/components/table/utils';
-export type { TableProps, TableRowProps, TableDataProps, TableHeaderProps };
-
-export default Object.assign(Table, { Row, Data, Header });
--- packages/core/components/table/utils.ts
+++
@@ -1,6 +0,0 @@
-export const TableShape = {
-  SQUARE: 'square',
-  ROUNDED: 'rounded',
-} as const;
-
-export type TableShape = (typeof TableShape)[keyof typeof TableShape];
